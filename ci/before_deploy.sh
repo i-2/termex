83f010e56a44dfa69664bd0e2018a07e6cd74f45
sourcepirate <sathyanarrayanan@yandex.com>
2018-09-12 23:55:40 +0800

    fix(ci/) trying to fix mac

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,13 +18,6 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    # TODO Update this to build the artifacts that matter to you
-    # cd termex_cli/ && cross rustc --bin termex_cli --target $TARGET --release -- -C lto
-
-    # cd ../termex_sync/ &&  cross rustc --bin termex_sync --target $TARGET --release -- -C lto
-
-    # # TODO Update this to package the right artifacts
-    # cd ..
     cp target/$TARGET/release/termex_cli $stage/
     cp target/$TARGET/release/termex_sync $stage
 
