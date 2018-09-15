6f85461359d33889e2acb3bce9995fb37f6b139c
sourcepirate <sathyanarrayanan@yandex.com>
2018-09-15 18:12:29 +0800

    fix(ci/) simpler ci is better

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -2,38 +2,24 @@
 
 set -ex
 
-main() {
-    local src=$(pwd) \
-          stage=
+os_type="$1"
 
-    case $TRAVIS_OS_NAME in
-        linux)
-            stage=$(mktemp -d)
-            ;;
-        osx)
-            stage=$(mktemp -d -t tmp)
-            ;;
-    esac
+main(){
 
-    test -f Cargo.lock || cargo generate-lockfile
+    # delete all
+    TAG=$TRAVIS_TAG
+    BIN_DIR="target/release/"
+    # target release
+    TERMEX_CLI="${BIN_DIR}termex_cli"
+    TERMEX_SYNC="${BIN_DIR}termex_sync"
 
-    # TODO Update this to build the artifacts that matter to you
-    cd termex_cli/
-    cross rustc --bin termex_cli --target $TARGET --release -- -C lto
+    mkdir -p /tmp/stage/
+    cp $TERMEX_CLI /tmp/stage/
+    cp $TERMEX_SYNC /tmp/stage/
+    # now tar the package and upload
+    tar -cvf ${CRATE_NAME}-${os_type}-${TRAVIS_TAG}.tar.gz /tmp/stage/
 
-    cd ../termex_sync/
-    cross rustc --bin termex_sync --target $TARGET --release -- -C lto
-
-    # TODO Update this to package the right artifacts
-    cd ..
-    cp target/$TARGET/release/termex_cli $stage/
-    cp target/$TARGET/release/termex_sync $stage
-
-    cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
-    cd $src
-
-    rm -rf $stage
 }
+
 
 main
